f4290cf834f44dddd8c646315534b92205fb178e
Elios <darioson@gmail.com>
2022-04-08 16:24:51 -0600

    friday from work

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 const fs = require('fs');
 const express = require('express');
-const res = require('express/lib/response');
+const { application } = require('express');
 
+/* Middlewares */
 const app = express();
 app.use(express.json());
-// app.get('/', (req, res) =>
-//   res
-//     .status(200)
-//     .json({ message: 'hello from the server side!', app: 'Natours' })
-// );
-// app.post('/', (req, res) => res.send('You can post to this endpoint...'));
+app.use((req, res, next) => {
+  console.log('hello from the middleware!');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res) =>
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } })
-);
-app.post('/api/v1/tours', (req, res) => {
+
+/*Functions handlers */
+/*Tours*/
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+const getOneTour = (req, res) => {
+  const id = req.params.id * 1; //by multiplying by 1, it converts the string into a number
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+};
+
+const postNewTour = (req, res) => {
   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -32,6 +53,73 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '<Updated tour here...>' } });
+};
+
+const deleteTour = (req, res) => {
+  res.status(204).json({ status: 'success', data: null });
+};
+
+/*Users*/
+const getAllUsers = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet implemented' });
+};
+
+const getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet implemented' });
+};
+
+const updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet implemented' });
+};
+
+const postUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet implemented' });
+};
+
+const deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet implemented' });
+};
+//app.get('/api/v1/tours', getAllTours);
+//app.get('/api/v1/tours/:id', getOneTour);
+//app.post('/api/v1/tours', postNewTour);
+//app.patch('/api/v1/tours/:id', updateTour);
+//app.delete('/api/v1/tours/:id', deleteTour);
+
+/*Routes */
+/*Tours */
+app.route('/api/v1/tours').get(getAllTours).post(postNewTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getOneTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+/*Users */
+app.route('/api/v1/users').get(getAllUsers).post(postUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
 const port = 3000;
 app.listen(port, () => console.log(`App is running on port ${port}...`));
