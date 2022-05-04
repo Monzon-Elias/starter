a7af33cf9cc5dab0a428452b3806426742623a42
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-05-03 21:49:13 -0600

    global error handler and more

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 /* Middle wares */
 const app = express();
 app.use(express.json());
@@ -10,18 +11,20 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('hello from the middleware!');
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 /*Mounting the Tours Route */
 app.use('/api/v1/tours', tourRouter);
-
 /*Mounting the Users Route */
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  //any parameter the next methods receives is an error. It'll skip all other middleware in the stack and send the error to the global error middleware, which will be executed. In this case there's no other middleware after this one, but if it were one or more, it will be skipped and go straight to the one with 4 parameters(error handler middleware)
+});
+
+//Middleware handling error
+app.use(globalErrorHandler);
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,11 @@
+//Middleware handling error
+// is characteristic of an error handler middleware to receive 4 parameters
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
