255ed233c0b0b0bba6e75dbf4190880adfac5351
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-05-05 21:57:08 -0600

    until 120

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,46 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error: don't leak error details to the clien
+  } else {
+    //1) Log error
+    console.error('ERROR 🤯', err);
+    //2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong 😩',
+    });
+  }
+};
 //Middleware handling error
 // is characteristic of an error handler middleware to receive 4 parameters
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  else if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
+  let error = { ...err };
+  if (error.name === 'CastError') error = handleCastErrorDB(error);
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  sendErrorProd(error, res);
 };
