cf497486029ef743bbe670f1aff6481c12dcb02a
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-05-08 00:04:46 -0600

    130 beginning

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -13,6 +14,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+
   res.status(201).json({
     status: 'success',
     token,
@@ -21,3 +23,18 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  //1. Check if email & password exist
+  if (!email || !password)
+    next(new AppError('Please provide email & password.', 400));
+  //2. Check if user exists & password is correct
+  //3. If everything is ok, send token to client
+  const token = '';
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,16 +5,16 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
-const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map((el) => el.message); //With Object.values we have access to the errors object that comes from mongoose.
-  const message = `Invalid input data ${errors.join('. ')}!`;
-  return new AppError(message, 400);
-};
-
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; //this regular expression matches text between quotes.
   console.log(value);
   const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message); //With Object.values we have access to the errors object that comes from mongoose.
+  const message = `Invalid input data ${errors.join('. ')}!`;
   return new AppError(message, 400);
 };
 
@@ -51,11 +51,13 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
-  else if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
-  let error = { ...err };
-  if (error.name === 'CastError') error = handleCastErrorDB(error);
-  if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-  if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+  else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
-  sendErrorProd(error, res);
+    sendErrorProd(error, res);
+  }
 };
