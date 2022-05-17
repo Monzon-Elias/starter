937f23eaa837e856d4a53e678bd25a7ba8c2a546
elias <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2022-05-16 21:49:35 -0600

    148

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-/* Middle wares */
+
 const app = express();
-app.use(express.json());
+///////////////////
+/* Global Middlewares */
+///////////////////
+
+//Set security HTTP headers (it ads a lot of security headers to the request)
+app.use(helmet());
+
+//Body parser, reading data from body into req.body with a limit in the payload
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against xss (malicious html with js inside)
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+//Limiting the amount of requests per hour
+const limiter = rateLimit({
+  max: 100, //100 requests from 1 IP (increase it if your app needs to do more requests)
+  windowMs: 60 * 60 * 1000, //1 hour
+  message: 'Too many request frim this IP, please try again in 1 hour!',
+});
+app.use('/api', limiter);
 
 /*Serving static files from the server */
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
