735f48074d7190d3d2b6ae2d19b4139bf915a732
Elios <darioson@gmail.com>
2022-05-26 16:00:25 -0600

    161 - copy the factory import - min 7:56

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true }); //now, things like 'POST /tour/2323/reviews' will be (*)
 router
-  .route('/')
+  .route('/') // (*) redirected to this route
   .get(reviewController.getAllReviews)
   .post(
     authController.protect,
